6cdfd77b9d9393aaa0c6a94547346e49a61d5347
Kanika Barik <89715447+Kaniii21@users.noreply.github.com>
2024-06-11 23:41:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (84.05%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,20 +1,26 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-     Map<Integer, Integer> indexMap = new HashMap<>();
-        for (int i = 0; i < arr2.length; i++) {
-            indexMap.put(arr2[i], i);
-        } 
-        Integer[] arr1Integer = Arrays.stream(arr1).boxed().toArray(Integer[]::new);
-        Arrays.sort(arr1Integer, (a, b) -> {
-            if (indexMap.containsKey(a) || indexMap.containsKey(b)) {
-                return indexMap.getOrDefault(a, arr2.length) - indexMap.getOrDefault(b, arr2.length);
+        int[] frequency = new int[1001];
+        for (int num : arr1) {
+            frequency[num]++;
+        }
+
+        int[] sortedArray = new int[arr1.length];
+        int index = 0;
+
+        for (int num : arr2) {
+            while (frequency[num] > 0) {
+                sortedArray[index++] = num;
+                frequency[num]--;
             }
-            return a - b;
-        });
-        
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = arr1Integer[i];
-        }  
-        return arr1;   
+        }
+
+        for (int num = 0; num < frequency.length; num++) {
+            while (frequency[num] > 0) {
+                sortedArray[index++] = num;
+                frequency[num]--;
+            }
+        }
+        return sortedArray;
     }
 }
