f81b2d7d916e5f7b1a2c1a6c609527ec4de7170f
Kanika Barik <89715447+Kaniii21@users.noreply.github.com>
2024-06-17 23:32:48 +0530

    Time: 3 ms (97.79%) | Memory: 40.5 MB (19.13%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+    long l = 0;
+    long r = (int) Math.sqrt(c);
+
+    while (l <= r) {
+      final long sum = l * l + r * r;
+      if (sum == c)
+        return true;
+      if (sum < c)
+        ++l;
+      else
+        --r;
+    }
+    return false;    
+    }
+}
